4e5edecda88faf5db33335fda4dd6f1911801a73
PeterKADam <89773385+PeterKADam@users.noreply.github.com>
2022-08-29 09:25:33 +0200

    hello n

--- src/hello-n.py
+++ src/hello-n.py
@@ -1,6 +1,8 @@
-
 # this asks the user for a number and turns the answer
 # into an integer
+
 n = int(input("How many times should I loop? "))
 
 # Now, print "Hello, World!" n times.
+for each in range(0,n):
+    print("Hello, World!")
