a21048cb86d43260ed9de847d5ead862a498aad9
PeterKADam <89773385+PeterKADam@users.noreply.github.com>
2022-08-30 11:03:10 +0200

    changes

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,11 @@
-
 # Print the pattern
+listy = []
+stop = 5
+for each in range(1, stop):
+    listy.append("*" * each)
+
+for each in range(stop, 0, -1):
+    listy.append("*" * each)
+
+print(listy)
+print(" ".join([x for x in listy]))
