2c5bc20efb1ff8c4fda4b31009a2b7c9f75357f2
PeterKADam <89773385+PeterKADam@users.noreply.github.com>
2022-09-02 08:38:10 +0200

    setsailforepicfail

--- src/pattern.py
+++ src/pattern.py
@@ -1,11 +1,16 @@
 # Print the pattern
+
+# this is impressivly terrible
+
 listy = []
 stop = 5
 for each in range(1, stop):
-    listy.append("*" * each)
+    listy.append(("* " * (each - 1)) + "*")
 
 for each in range(stop, 0, -1):
-    listy.append("*" * each)
+    listy.append(("* " * (each - 1)) + "*")
 
-print(listy)
-print(" ".join([x for x in listy]))
+# print(listy)
+for each in listy:
+    print(each)
+# print(" ".join([x for x in listy]) + "\n")
